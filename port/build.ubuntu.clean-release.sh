b6a777bcfe7804036a5bbdb6d6852ae0c7f878b9
Grigore Stefan <g_stefan@yahoo.com>
2021-01-03 04:03:14 +0200

    release build

--- port/build.ubuntu.clean-release.sh
+++ port/build.ubuntu.clean-release.sh
@@ -3,6 +3,6 @@
 # http://unlicense.org/
 # Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo "-> clean-local-release quantum-script-extension-url"
+echo "-> clean-release quantum-script-extension-url"
 
 rm -rf release
--- port/build.ubuntu.release.sh
+++ port/build.ubuntu.release.sh
@@ -3,11 +3,11 @@
 # http://unlicense.org/
 # Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo "-> local-release quantum-script-extension-url"
+echo "-> release quantum-script-extension-url"
 
 cmdX(){
 	if ! "$@" ; then
-		echo "Error: local-release"
+		echo "Error: release"
 		exit 1
 	fi
 }
